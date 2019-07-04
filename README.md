17f84fe45d8b58fbf92542f26ef1297a4634dfb6
John Kossa <kg959ster@gmail.com>
2019-07-03 23:05:20 -0500

    Initial Commit. Solves a single equation and quits when it determines there's nothing else to solve for.

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
-# planetary-calculator
-A program that solves systems of equations to narrow down infinite ranges into feasible ones.
+# Object types
+## Variable Bag
+Contains a bag of all variables for the object
+
+## Variable
+### Types
+* Given
+* Derived
+* Assumed
+
+Derived variables should contain a list of "sources" which will map to equations
+
+### Value Types
+Has a value type:
+
+* Unconstrained Range (-inf, inf)
+* Constrained Range (someX, someY)
+* Finite (X)
+
+#### Value Type Hierarchy
+First by value type
+* Finite
+* Constrained Range
+* Unconstrained Range
+
+Constrained ranges have a hierarchy inverse to the "size" of their ranges, so smaller ranges are more "correct"
+
+### Unit
+Has a unit. Standard SI units should be used if possible.
+
+### Name
+
+Variables should have a unique name not shared by any other variable in the bag
+
+## Equation
+### Manifest
+Equations should have a manifest specifying what variables it contains and a unique name.
+### Documentation
+Should be specified in comments in its most recognizable form in the comments
+
+Possibly specify a web link if pulled from an obscure source
+
+Units should be specified in comments whenever possible
+
+### Behavior
+#### Applicability test
+Equation should be applied if:
+* the bag contains all variables listed in the equation manifest or all but one
+* no more than one variable is an "unconstrained range"
+
+The equation should attempt to solve for the lowest variable in the value type hierarchy.
+
+* If the lowest in the hierarchy is a given, pick the next lowest.
+* If the lowest in the hierarchy is derived and the source is from this equation, pick the next lowest.
+* If after both of the above rules have been applied there are no remaining canidates, the equation does not apply.
+
+#### Method of calculation
+1. Pick the form of the equation that yields the target variable
+2. For each variable that is
+* finite, use the value directly for all calculations
+* constrained range, do one calculation for the min of the range and one for the max of the range
+* unconstrained range, if you're here you should have failed the applicability test
+3. For N constrained range variables, we should have 2^N possible solutions
+4. If any of the variables are constrained ranges, the result constrained range is min(individual results),max(individual results)
+5. If none of the variables are constrained ranges, the answer is finite.
+6. If a range has the same min and max value, convert it to a finite value instead.
+7. Update the target variable
+
+If a range result has the
+
+* If the target is finite and the result is a range, you did something wrong
+* If the target is a range and the result is finite
+  * If the range does not intersect with the result
+    * Target is derived -> calculation failure
+    * Target is assumed -> replace target's value with result and upgrade it to "derived", notify user of an "assumption failure"
+  * If the range does intersect with the result -> update the value with the finite result and set the variable to "derived"
+* If both the target and the result are a range
+  * If the range does not intersect with the result
+    * Target is derived -> calculation failure
+    * Target is assumed -> replace target's value with result and upgrade it to "derived", notify user of an "assumption failure"
+  * If the target fully overlaps or is fully overlapped by the result
+    * Set target to the smallest of the two ranges.
+      * If the result was the smallest range, set the source to the equation
+      * If the target had the smallest range, leave the source what it was, note on the cycle to exclude this variable from the list of valid targets for that equation for the rest of the cycle.
+  * If the target partially overlaps with the result
+    * New range is (max(targetMin, resultMin), min(targetMax, resultMax)
+    * If source is a
+      * string identifying an equation, convert it to a list with both equations in it
+      * list with more than one string, add the new source to the list
+8. If the updated variable is a range with the same min and max, convert it to a finite value.
+
+##### Possible improvements
+
+Realistically, if the range source is an intersection of 3 sources, there might be more constraining that could be done if we were to rerun the other source equations with the updated information.
+
+Possibly take samples in the middle of the range? This could help with multimodal equations.
+
+
+#### Calculation Failure
+A calculation failure occurs if the target variable is given or derived and does not intersect with the result value.
+
+If this is the case, notify the user and cease calculation.
+
+# Workflow
+User will start with a blank set of variables in their bag.
+
+User may pick a set of assumptions to kick start the calculations. This kicks off the process in "adding a variable"
+
+## Adding a variable
+Each time the user changes a variable, clear any restricted variable-equation pairs and start a calculation cycle.
+
+## Removing a variable (Unfinished)
+The user can un-set any givens or assumptions in their bag. This clears any derived values from the bag and kicks off the calculation cycle again.
+
+## Calculation Cycle
+Check each equation for viability. Do not begin calculations until all have been checked.
+
+If there are no viable equations, we're done.
+
+Otherwise, for each viable equation, order the targets by hierarchy.
+
+Perform the equations in the order of their hierarchy.
+
+Equations which cause a calculation failure do not update the target variable and immediately stop the calculation cycle.
+
+Equations which fail to update a variable (usually because of a reselected target or a double overlapping range) are tagged with an exclusion for that variable.
+
+After all equations have been run, start another calculation cycle. We will keep rechecking the calculation cycle until there are no viable equations.
+
---
+++ planet_calc.js
@@ -0,0 +1,264 @@
+const VARIABLE_TYPE = {
+	GIVEN: 1,
+	DERIVED: 2,
+	ASSUMED: 3
+};
+
+const VALUE_TYPE = {
+	FINITE: 3,
+	CONSTRAINED_RANGE: 2,
+	UNCONSTRAINED_RANGE: 1
+};
+
+function getValueTypeFromValue(value){
+	if(!Array.isArray(value)){
+		return VALUE_TYPE.FINITE;
+	}
+	if(value[0] === -Infinity && value[1] === Infinity){
+		return VALUE_TYPE.UNCONSTRAINED_RANGE;
+	}
+	return VALUE_TYPE.CONSTRAINED_RANGE;
+}
+
+class VariableBag{
+	constructor(){
+		this.variables = {}
+	}
+	addVariable(variable){
+		//check to see if bag contains another variable by that name
+		this.variables[variable.name] = variable;
+	}
+	containsVariable(variableName){
+		return Object.keys(this.variables).indexOf(variableName) >= 0;
+	}
+}
+
+class Variable{
+	constructor(type, unit, name) {
+		this.value = [-Infinity, Infinity];
+		this.type = type;
+		this.valueType = VALUE_TYPE.UNCONSTRAINED_RANGE;
+		this.unit = unit;
+		this.name = name;
+	}
+
+	updateValue(newValueType, newValue){
+		if(this.valueType === VALUE_TYPE.FINITE)
+			throw "Value is finite, update is invalid";
+		if(newValueType === VALUE_TYPE.UNCONSTRAINED_RANGE)
+			throw "New value is unconstrained. Update is invalid";
+		if(newValueType === VALUE_TYPE.FINITE){
+			this.valueType = VALUE_TYPE.FINITE;
+			this.value = newValue;
+			return true;
+		}
+		if(newValueType === VALUE_TYPE.CONSTRAINED_RANGE){
+			this.valueType = VALUE_TYPE.CONSTRAINED_RANGE;
+			let oldValue = [...this.value];
+			this.value[0] = Math.max(this.value[0], newValue[0]);
+			this.value[1] = Math.min(this.value[1], newValue[1]);
+			console.log("updateValue", this.value, oldValue);
+			if(oldValue[0] !== this.value[0] || oldValue[1] !== this.value[1]){
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
+class Equation{
+	constructor(variables, forms, name){
+		this.variables = variables;
+		this.forms = forms;
+		this.name = name;
+	}
+	applies(variableBag){
+		/*
+		* Accepts:
+		*   variablebag: the variable bag
+		* Returns:
+		*   boolean
+		*
+		* Compare variable manifest with bag
+		* Applies if bag contains all variables in manifest or all variables but one
+		* */
+
+		let diffSet = this.variables.filter(x => !Object.keys(variableBag.variables).includes(x));
+		let hasEnoughVariables = diffSet.length <=1;
+		//TODO check to make sure computable variables are not already finite
+		console.log(`Equation ${this.name} differs by [${diffSet}] and ${hasEnoughVariables?'has enough variables':"doesn't have enough variables"}`);
+
+		return hasEnoughVariables
+	}
+	chooseForm(variableBag){
+		/*
+		* Accepts:
+		*   variableBag: the variable bag
+		* Returns:
+		*   {target, form}
+		* If one variable not present in bag, choose form that yields that variable
+		* If all variables are present, pick via VALUE_TYPE hierarchy
+		* If all match, return null;
+		* */
+		console.log("Choose form called", this);
+		let diffSet = this.variables.filter(x => !Object.keys(variableBag.variables).includes(x));
+		if(diffSet.length === 0){
+			let variables = Object.values(variableBag.variables).filter(x => this.variables.includes(x.name));
+			variables.sort(x => x.valueType);
+			if(variables[0].valueType === VALUE_TYPE.FINITE){
+				throw Error(`In Equation.chooseForm: Value type is FINITE, this function fails its precondition`);
+			}else if(variables[0].valueType === VALUE_TYPE.UNCONSTRAINED_RANGE){
+				return {
+					target: variables[0].name,
+					form: this.forms[variables[0].name],
+					equation: this
+				}
+			}else{
+				console.log("variables", variables);
+				variables.sort((a,b)=> Math.abs(a.value[1]-a.value[0]) - Math.abs(b.value[1]-b.value[0]));
+				//sort by minimal range
+				return {
+					target: variables[0].name,
+					form: this.forms[variables[0].name],
+					equation: this
+				}
+			}
+			//get the variable(s) with the minimum value type
+			//if minimal value type is unconstrained, just pick the first one
+			//if minimal value type is constrained take the one with the lowest numerical range TODO this is not perfect considering unit mismatches, but it'll have to do for now
+			//if minimal value type is finite, throw an error
+			//
+		}else if(diffSet.length === 1){
+			if(!this.forms[diffSet[0]]){
+				throw Error(`Equation ${this.name} has no form for ${diffSet[0]}`)
+			}
+			return {target: diffSet[0], form: this.forms[diffSet[0]], equation: this};
+		}else{
+			throw Error(`Diff set is too large to continue (Equation.chooseForm) ${this.name}, ${this.variables}`)
+		}
+	}
+	compute(form, variableBag){
+
+		/*
+		* for each finite variable, insert into the set
+		* for each constrained variable, copy each set and set the min as the value in one and the max as the value in the other
+		* run the equation for each set, storing the value for each
+		* if only 1 set, update that value as finite
+		* if multiple sets, update the value as a constrained range using the [Min of all values, max of all values]
+		* */
+		let sets = [{}];
+		for(let variable of Object.values(variableBag.variables)){
+			switch(variable.valueType) {
+				case VALUE_TYPE.FINITE:
+					sets.forEach(el => el[variable.name] = variable.value);
+					break;
+				case VALUE_TYPE.CONSTRAINED_RANGE:
+					let setForkA = JSON.parse(JSON.stringify(sets));
+					setForkA.forEach(el => el[variable.name] = variable.value[0]);
+					let setForkB = JSON.parse(JSON.stringify(sets));
+					setForkB.forEach(el => el[variable.name] = variable.value[1]);
+					sets = [...setForkA, ...setForkB];
+					break;
+				case VALUE_TYPE.UNCONSTRAINED_RANGE:
+					throw  "A variable with an unconstrained range value type was passed to Equation.compute. Cannot continue"+variable;
+			}
+		}
+
+		//grab the result(s) and return them
+		let results = [];
+		for(let variableSet of sets){
+			results.push(form(variableSet))
+		}
+		results = [...new Set(results)]; //exclude duplicates
+		let resultMin = results.reduce((agg, el) => Math.min(agg, el), results[0]);
+		let resultMax = results.reduce((agg, el) => Math.max(agg, el), results[0]);
+		if(resultMin === resultMax){
+			return resultMin;
+		}
+		return [resultMin, resultMax];
+	}
+}
+
+let myEquations = {
+	density: new Equation(["radius", "mass", "density"],{
+		density: ({mass, radius})=> mass / (4/3 * Math.PI * Math.pow(radius,3)),
+		radius: ({density, mass}) => Math.cbrt( mass / density * 3/4 / Math.PI ),
+		mass: ({}) => {throw "Not Implemented"}
+	}, "sphericalDensity")
+};
+let myBag = new VariableBag();
+myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "km", "radius"));
+myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "kg", "mass"));
+//myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "kg/m^3", "density"));
+myBag.variables["radius"].updateValue(VALUE_TYPE.CONSTRAINED_RANGE, [1, 200]);
+myBag.variables["mass"].updateValue(VALUE_TYPE.CONSTRAINED_RANGE, [100, 1000]);
+
+function computeCycle(){
+	let excludedEquations = [];
+	let keepGoing;
+	let cycleCount = 1;
+	do{
+		keepGoing = true;
+		let viableEquations = [];
+		for(equationName in myEquations){
+			if(equationName in excludedEquations){
+				continue;
+			}
+			if(myEquations[equationName].applies(myBag)){
+				viableEquations.push(equationName);
+			}
+		}
+		if(!viableEquations.length){
+			keepGoing = false;
+		}
+		console.log("viableEquations", viableEquations);
+		let chosenForms = viableEquations.map(equationName => myEquations[equationName].chooseForm(myBag));
+		//TODO sort by bag value type hierarchy
+		console.log("RESULTS");
+		let computeResults = chosenForms.map((el, i, arr)=> {
+			return {...el, target: el.target, value: el.equation.compute(el.form, myBag)}
+		});
+		console.log(computeResults);
+		computeResults = computeResults.map((el, i, arr) => {
+			return {...el, valueType: getValueTypeFromValue(el.value)}
+		});
+		let valuesUpdated = computeResults.map((el, i, arr) => {
+			if(myBag.containsVariable(el.target)){
+				return {...el, updated: myBag.variables[el.target].updateValue(el.valueType, el.value)}
+			}else{
+				console.log("new variable created");
+				//console.log(JSON.stringify(myBag.variables));
+				console.log(Object.keys(myBag.variables));
+				let myNewVariable = new Variable(VARIABLE_TYPE.DERIVED,"unknown", el.target);
+				console.log(myNewVariable);
+				//console.log(JSON.stringify(myNewVariable));
+				//myBag.addVariable(new Variable(VARIABLE_TYPE.DERIVED,"unknown", el.target));
+				myBag.addVariable(myNewVariable);
+				//console.log(JSON.stringify(myBag.variables));
+				console.log(Object.keys(myBag.variables));
+				return {...el, updated: myBag.variables[el.target].updateValue(el.valueType, el.value)}
+			}
+		});
+		console.log("valuesUpdated", valuesUpdated[0]);
+		//for each that did not produce an update, add it to the list of excluded equations
+		let notUpdated = valuesUpdated.filter(el => !el.updated);
+		console.log("notUpdated", notUpdated);
+		notUpdated.forEach((el, i, arr)=>{
+			console.log(`adding ${el.equation.name} to list of excluded equations`);
+			console.log(el);
+			excludedEquations.push(el.equation.name);
+		});
+		console.log("bag state", myBag);
+		if(valuesUpdated.every(el => !el.updated)){
+			console.log("no updates occurred, exiting early");
+			keepGoing = false;
+		}
+		//sort viable equations by value type hierarchy and execute
+		//if no value update, add equation to the excludedEquations list
+		console.log(`cycle ${cycleCount} completed`);
+		cycleCount++;
+	}while(keepGoing && cycleCount < 10)
+}
+
+
+computeCycle();
