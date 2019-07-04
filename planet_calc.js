cac52aa7ae2cf5566ae488c96093252937da8ef1
JohnKossa <kg959ster@gmail.com>
2019-07-03 23:51:09 -0500

    Code cleanup

--- planet_calc.js
+++ planet_calc.js
@@ -20,16 +20,24 @@ function getValueTypeFromValue(value){
 	return VALUE_TYPE.CONSTRAINED_RANGE;
 }
 
+function removeDuplicates (list){
+	return [...new Set(list)];
+}
+
+function getValueRange(variable){
+	return Math.abs(variable.value[1]-variable.value[0])
+}
+
 class VariableBag{
 	constructor(){
 		this.variables = {}
 	}
 	addVariable(variable){
-		//check to see if bag contains another variable by that name
+		//TODO check to see if bag contains another variable by that name
 		this.variables[variable.name] = variable;
 	}
 	containsVariable(variableName){
-		return Object.keys(this.variables).indexOf(variableName) >= 0;
+		return variableName in this.variables;
 	}
 }
 
@@ -57,7 +65,6 @@ class Variable{
 			let oldValue = [...this.value];
 			this.value[0] = Math.max(this.value[0], newValue[0]);
 			this.value[1] = Math.min(this.value[1], newValue[1]);
-			console.log("updateValue", this.value, oldValue);
 			if(oldValue[0] !== this.value[0] || oldValue[1] !== this.value[1]){
 				return true;
 			}
@@ -98,36 +105,32 @@ class Equation{
 		*   {target, form}
 		* If one variable not present in bag, choose form that yields that variable
 		* If all variables are present, pick via VALUE_TYPE hierarchy
+		* 	if minimal value type is unconstrained, just pick the first one
+		*	if minimal value type is constrained take the one with the lowest numerical range TODO this is not perfect considering unit mismatches, but it'll have to do for now
+		*	if minimal value type is finite, throw an error
 		* If all match, return null;
 		* */
-		console.log("Choose form called", this);
 		let diffSet = this.variables.filter(x => !Object.keys(variableBag.variables).includes(x));
 		if(diffSet.length === 0){
 			let variables = Object.values(variableBag.variables).filter(x => this.variables.includes(x.name));
 			variables.sort(x => x.valueType);
-			if(variables[0].valueType === VALUE_TYPE.FINITE){
-				throw Error(`In Equation.chooseForm: Value type is FINITE, this function fails its precondition`);
-			}else if(variables[0].valueType === VALUE_TYPE.UNCONSTRAINED_RANGE){
-				return {
-					target: variables[0].name,
-					form: this.forms[variables[0].name],
-					equation: this
-				}
-			}else{
-				console.log("variables", variables);
-				variables.sort((a,b)=> Math.abs(a.value[1]-a.value[0]) - Math.abs(b.value[1]-b.value[0]));
-				//sort by minimal range
-				return {
-					target: variables[0].name,
-					form: this.forms[variables[0].name],
-					equation: this
-				}
+			switch(variables[0].valueType){
+				case VALUE_TYPE.FINITE:
+					throw Error(`In Equation.chooseForm: Value type is FINITE, this function fails its precondition`);
+				case VALUE_TYPE.UNCONSTRAINED_RANGE:
+					return {
+						target: variables[0].name,
+						form: this.forms[variables[0].name],
+						equation: this
+					};
+				case VALUE_TYPE.CONSTRAINED_RANGE:
+					variables.sort((a,b)=> getValueRange(a) - getValueRange(b));
+					return {
+						target: variables[0].name,
+						form: this.forms[variables[0].name],
+						equation: this
+					}
 			}
-			//get the variable(s) with the minimum value type
-			//if minimal value type is unconstrained, just pick the first one
-			//if minimal value type is constrained take the one with the lowest numerical range TODO this is not perfect considering unit mismatches, but it'll have to do for now
-			//if minimal value type is finite, throw an error
-			//
 		}else if(diffSet.length === 1){
 			if(!this.forms[diffSet[0]]){
 				throw Error(`Equation ${this.name} has no form for ${diffSet[0]}`)
@@ -138,7 +141,6 @@ class Equation{
 		}
 	}
 	compute(form, variableBag){
-
 		/*
 		* for each finite variable, insert into the set
 		* for each constrained variable, copy each set and set the min as the value in one and the max as the value in the other
@@ -169,7 +171,7 @@ class Equation{
 		for(let variableSet of sets){
 			results.push(form(variableSet))
 		}
-		results = [...new Set(results)]; //exclude duplicates
+		results = removeDuplicates(results); //exclude duplicates
 		let resultMin = results.reduce((agg, el) => Math.min(agg, el), results[0]);
 		let resultMax = results.reduce((agg, el) => Math.max(agg, el), results[0]);
 		if(resultMin === resultMax){
@@ -180,18 +182,15 @@ class Equation{
 }
 
 let myEquations = {
-	density: new Equation(["radius", "mass", "density"],{
+	//TODO having both a key and a name property is overdefinition
+	sphericalDensity: new Equation(["radius", "mass", "density"],{
 		density: ({mass, radius})=> mass / (4/3 * Math.PI * Math.pow(radius,3)),
 		radius: ({density, mass}) => Math.cbrt( mass / density * 3/4 / Math.PI ),
 		mass: ({}) => {throw "Not Implemented"}
 	}, "sphericalDensity")
 };
+
 let myBag = new VariableBag();
-myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "km", "radius"));
-myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "kg", "mass"));
-//myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "kg/m^3", "density"));
-myBag.variables["radius"].updateValue(VALUE_TYPE.CONSTRAINED_RANGE, [1, 200]);
-myBag.variables["mass"].updateValue(VALUE_TYPE.CONSTRAINED_RANGE, [100, 1000]);
 
 function computeCycle(){
 	let excludedEquations = [];
@@ -211,14 +210,11 @@ function computeCycle(){
 		if(!viableEquations.length){
 			keepGoing = false;
 		}
-		console.log("viableEquations", viableEquations);
 		let chosenForms = viableEquations.map(equationName => myEquations[equationName].chooseForm(myBag));
-		//TODO sort by bag value type hierarchy
-		console.log("RESULTS");
+		//TODO sort by bag value type hierarchy?
 		let computeResults = chosenForms.map((el, i, arr)=> {
 			return {...el, target: el.target, value: el.equation.compute(el.form, myBag)}
 		});
-		console.log(computeResults);
 		computeResults = computeResults.map((el, i, arr) => {
 			return {...el, valueType: getValueTypeFromValue(el.value)}
 		});
@@ -226,39 +222,29 @@ function computeCycle(){
 			if(myBag.containsVariable(el.target)){
 				return {...el, updated: myBag.variables[el.target].updateValue(el.valueType, el.value)}
 			}else{
-				console.log("new variable created");
-				//console.log(JSON.stringify(myBag.variables));
-				console.log(Object.keys(myBag.variables));
-				let myNewVariable = new Variable(VARIABLE_TYPE.DERIVED,"unknown", el.target);
-				console.log(myNewVariable);
-				//console.log(JSON.stringify(myNewVariable));
-				//myBag.addVariable(new Variable(VARIABLE_TYPE.DERIVED,"unknown", el.target));
-				myBag.addVariable(myNewVariable);
-				//console.log(JSON.stringify(myBag.variables));
-				console.log(Object.keys(myBag.variables));
+				myBag.addVariable(new Variable(VARIABLE_TYPE.DERIVED,"unknown", el.target));
 				return {...el, updated: myBag.variables[el.target].updateValue(el.valueType, el.value)}
 			}
 		});
-		console.log("valuesUpdated", valuesUpdated[0]);
 		//for each that did not produce an update, add it to the list of excluded equations
 		let notUpdated = valuesUpdated.filter(el => !el.updated);
-		console.log("notUpdated", notUpdated);
 		notUpdated.forEach((el, i, arr)=>{
-			console.log(`adding ${el.equation.name} to list of excluded equations`);
-			console.log(el);
+			console.log(`Adding ${el.equation.name} to list of excluded equations`);
 			excludedEquations.push(el.equation.name);
 		});
-		console.log("bag state", myBag);
 		if(valuesUpdated.every(el => !el.updated)){
-			console.log("no updates occurred, exiting early");
+			console.log("No updates occurred, exiting early");
 			keepGoing = false;
 		}
 		//sort viable equations by value type hierarchy and execute
 		//if no value update, add equation to the excludedEquations list
-		console.log(`cycle ${cycleCount} completed`);
+		console.log(`Cycle ${cycleCount} completed`);
 		cycleCount++;
 	}while(keepGoing && cycleCount < 10)
 }
 
-
+myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "km", "radius"));
+myBag.addVariable(new Variable(VARIABLE_TYPE.GIVEN, "kg", "mass"));
+myBag.variables["radius"].updateValue(VALUE_TYPE.CONSTRAINED_RANGE, [1, 200]);
+myBag.variables["mass"].updateValue(VALUE_TYPE.CONSTRAINED_RANGE, [100, 1000]);
 computeCycle();
